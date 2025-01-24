16489fe20c5a3d7f7f1629562d1c08517b1da8ee
Mahu <chong.jiang@transwarp.io>
2025-01-24 11:33:48 +0800

    两数之后,只用一次循环

--- 04-两数之和.js
+++ 04-两数之和.js
@@ -3,7 +3,7 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+/*var twoSum = function(nums, target) {
     let arr = []
     for (let i = 0; i < nums.length-1; i++) {
         for (let j = i+1; j < nums.length; j++) {
@@ -15,5 +15,16 @@ var twoSum = function(nums, target) {
         }
     }
 
+};*/
+var twoSum = function(nums, target) {
+    let obj ={}
+    for (let i = 0; i < nums.length; i++) {
+        let res = target - nums[i]
+        if (obj[res]!==undefined) {
+            return [obj[res], i]
+        }else{
+            obj[nums[i]] = i
+        }
+    }
 };
 console.log(twoSum([2,7,11,15], 9),'123')
