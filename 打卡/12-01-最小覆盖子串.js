0a871c37b11f6d58116875e1fbae107d61d1e097
Mahu <chong.jiang@transwarp.io>
2025-02-08 01:39:45 +0800

    困难-最小覆盖子串

--- 打卡/12-01-最小覆盖子串.js
+++ 打卡/12-01-最小覆盖子串.js
@@ -6,42 +6,70 @@
  * https://leetcode.cn/problems/minimum-window-substring/
  */
 var minWindow = function(s, t) {
-    // ai自动生成
-    let left = 0;
-    let right = 0;
-    let start = 0;
-    let minLen = Infinity;
-    let window = {};
-    let needs = {};
-    let match = 0;
-    for (let i = 0; i < t.length; i++) {
-        needs[t[i]] = (needs[t[i]] || 0) + 1;
+    if(s.length===t.length){
+        return t
+    }else if (s.length<t.length){
+        return ''
     }
-    let needsLen = Object.keys(needs).length;
-    while (right < s.length) {
-        let c1 = s[right];
-        if (needs[c1]) {
-            window[c1] = (window[c1] || 0) + 1;
-            if (window[c1] === needs[c1]) {
-                match++;
+    let i=0,j=0
+    let minLen = s
+    let set = new Set()//存abc的,不是完整
+
+    let str=''
+    let arr = []
+
+    const mySet = new Set(t);
+
+    let flag = false
+
+
+    while(j<s.length){
+        if ((!set.size && t.includes(s[j])) || set.size<mySet.size) {//存abd的池子是空的;t有,并且没进set
+            if (t.includes(s[j])) {
+                set.add(s[j]);//只存abc
             }
-        }
-        right++;
-        while (match === needsLen) {
-            if (right - left < minLen) {
-                start = left;
-                minLen = right - left;
-            }
-            let c2 = s[left];
-            if (needs[c2]) {
-                window[c2]--;
-                if (window[c2] < needs[c2]) {
-                    match--;
+
+            arr.push(s[j]);
+            let flag1  = false;
+            for (let k = 0; k < t.length; k++) {
+                if (!set.has(t[k])) {//t是不是在set都出现过了
+                    flag1 = true
+                    break
                 }
             }
-            left++;
+            if (!flag1) {
+                //t中所有的值都在set出现过,说明可以收网了
+                minLen = minLen.length < arr.length ? minLen : arr.join('');
+                // console.log(arr.toString());
+                // arr=[]
+                // arr.push(s[j])
+                // set=new Set(s[j])
+                // i=j//先不一个一个滑动了
+                // flag1 = false
+                flag=true
+                set.delete(s[i])
+            }
+            j++;
+
         }
+        while (flag){
+            //移动i,窗口左指针
+            arr.shift()
+            i++
+            if (set.has(s[i])) {
+                // set=new Set()
+                flag=false
+                j=i
+                arr=[]
+            }
+        }
+        /*else if (set.has(s[j])) {//重复,滑动 左推拉门
+            set.delete(s[i++]);
+        }*/
+
+
     }
-    return minLen === Infinity ? '' : s.substr(start, minLen);
+    return minLen
 };
 console.log(minWindow("ADOBECODEBANC","ABC"))//BANC
+// console.log(minWindow("a","aa"))//BANC
